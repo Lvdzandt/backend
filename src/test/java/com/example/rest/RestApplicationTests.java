4a700146161a6f40ea7b2ff5d96ece9882fdcbbe
Lvzandt <lucvdzandt@gmail.com>
2022-02-23 11:53:33 +0100

    fixed tests

--- src/test/java/com/example/rest/RestApplicationTests.java
+++ src/test/java/com/example/rest/RestApplicationTests.java
@@ -1,9 +1,23 @@
 package com.example.rest;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
-@SpringBootTest
+import javax.transaction.Transactional;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(
+        classes = { JPAConfig.class },
+        loader = AnnotationConfigContextLoader.class)
+@Transactional
 class RestApplicationTests {
 
     @Test
